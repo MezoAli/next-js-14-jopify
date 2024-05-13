65211b11b62e95b131cec5321bb42bfe1d783c29
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-05-13 10:48:11 +0300

    create Navbar

--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -1,20 +1,19 @@
 import Image from "next/image";
-import Logo from "../../assets/logo.svg";
 import ToggleTheme from "./ToggleTheme";
 import { UserButton } from "@clerk/nextjs";
+import DropdownLinks from "./DropdownLinks";
 
 const Navbar = () => {
   return (
-    // <div className="bg-slate-700 w-full lg:grid-cols-4">
-    //   <div className="max-w-7xl mx-auto flex justify-between py-8 mb-4">
-    //     <Image src={Logo} alt="logo" />
-    //     <div className="flex justify-center items-center gap-4">
-    //       <ToggleTheme />
-    //       <UserButton />
-    //     </div>
-    //   </div>
-    // </div>
-    <h1>navbar</h1>
+    <nav className="bg-muted py-4 sm:px-16 lg:px-24 px-4 flex items-center justify-between">
+      <div>
+        <DropdownLinks />
+      </div>
+      <div className="flex justify-center items-center gap-4">
+        <ToggleTheme />
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </nav>
   );
 };
 
