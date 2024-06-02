632dd8754b36639b70b8d6b08894bf337ea01885
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-06-02 20:52:18 +0300

    create CreateForm comp with all data needed

--- components/pages/CreateJobForm.tsx
+++ components/pages/CreateJobForm.tsx
@@ -8,7 +8,6 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -16,18 +15,38 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import toast from "react-hot-toast";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 const FormSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+  position: z.string().min(10, {
+    message: "Position must be at least 10 characters.",
   }),
+  company: z.string().min(3, {
+    message: "Company must be at least 3 characters.",
+  }),
+  location: z.string().min(3, {
+    message: "Location must be at least 3 characters.",
+  }),
+  jobStatus: z.enum(["pending", "rejected", "interview"]),
+  jobMode: z.enum(["full-time", "part-time", "intership"]),
 });
 
 export default function CreateJobForm() {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      username: "",
+      position: "",
+      company: "",
+      location: "",
+      jobStatus: "pending",
+      jobMode: "full-time",
     },
   });
 
@@ -40,30 +59,136 @@ export default function CreateJobForm() {
     //     </pre>
     //   ),
     // });
-    toast.success("successful");
+    toast.success(JSON.stringify(data, null, 2));
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem className="flex flex-col gap-4">
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <div className="max-w-5xl mx-auto mt-16 bg-muted p-7 rounded-[5px]">
+      <h2 className="text-4xl font-bold mb-6">Add Job</h2>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="grid lg:grid-cols-3 gap-4 mb-8">
+            <FormField
+              control={form.control}
+              name="position"
+              render={({ field }) => (
+                <FormItem className="flex flex-col gap-2">
+                  <FormLabel className="text-lg font-semibold">
+                    Position
+                  </FormLabel>
+                  <FormControl>
+                    <Input {...field} className="rounded-[5px]" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="company"
+              render={({ field }) => (
+                <FormItem className="flex flex-col gap-2">
+                  <FormLabel className="text-lg font-semibold">
+                    Company
+                  </FormLabel>
+                  <FormControl>
+                    <Input {...field} className="rounded-[5px]" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="location"
+              render={({ field }) => (
+                <FormItem className="flex flex-col gap-2">
+                  <FormLabel className="text-lg font-semibold">
+                    Location
+                  </FormLabel>
+                  <FormControl>
+                    <Input {...field} className="rounded-[5px]" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="grid lg:grid-cols-3 gap-4">
+            <FormField
+              control={form.control}
+              name="jobStatus"
+              render={({ field }) => (
+                <FormItem className="flex flex-col gap-2">
+                  <FormLabel className="text-lg font-semibold">
+                    Job Status
+                  </FormLabel>
+                  <FormControl>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <SelectTrigger className="w-full rounded-[5px]">
+                        <SelectValue />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          {["pending", "rejected", "interview"].map((item) => {
+                            return (
+                              <SelectItem key={item} value={item}>
+                                {item}
+                              </SelectItem>
+                            );
+                          })}
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="jobMode"
+              render={({ field }) => (
+                <FormItem className="flex flex-col gap-2">
+                  <FormLabel className="text-lg font-semibold">
+                    Job Mode
+                  </FormLabel>
+                  <FormControl>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <SelectTrigger className="w-full rounded-[5px]">
+                        <SelectValue />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          {["full-time", "part-time", "intership"].map(
+                            (item) => {
+                              return (
+                                <SelectItem key={item} value={item}>
+                                  {item}
+                                </SelectItem>
+                              );
+                            }
+                          )}
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="rounded-[10px] capitalize mt-auto">
+              create job
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
   );
 }
