715632fb09fa6a0b23aed3f4d4282c5cf8a498bf
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-07-05 16:23:51 +0300

    testing submitting empty form and showing error messages

--- __tests__/CreateJobForm.test.tsx
+++ __tests__/CreateJobForm.test.tsx
@@ -1,8 +1,7 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import CreateJobForm from "../components/pages/CreateJobForm";
 import { render, screen } from "@testing-library/react";
-import { useRouter } from "next/router";
 import { expect, describe, test, vi } from "vitest";
+import userEvents from "@testing-library/user-event";
 
 vi.mock("next/navigation", async () => {
   return {
@@ -19,10 +18,30 @@ vi.mock("@tanstack/react-query", async () => {
   };
 });
 
+const data = {
+  position: "test position",
+  company: "test company",
+  location: "test location",
+  jobStatus: "pending",
+  jobMode: "full-time",
+};
+
 describe("testing createJobForm", () => {
   test("testing createJobForm ui", async () => {
     render(<CreateJobForm />);
     const title = screen.getByTestId("title");
     expect(title).toBeDefined();
+  });
+
+  test("testing submitting empty form", async () => {
+    render(<CreateJobForm />);
+    const Btns = screen.getAllByRole("button");
+    expect(Btns[1].innerHTML).toBe("create job");
+    await userEvents.click(Btns[1]);
+    const paragrapghs = await screen.findAllByRole("paragraph");
+    expect(paragrapghs).toHaveLength(3);
+    expect(paragrapghs[0].innerHTML).toBe(
+      "Position must be at least 10 characters."
+    );
   });
 });
